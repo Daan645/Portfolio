5f4c7c60d0b0c6aba8915278d8af08736d0dd768
Daan Jacobs <daan@videotechniek.nl>
2024-08-04 23:08:46 +0200

    Programming language section
    made the section where i display the programing languages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,58 @@
-<style>
-@import url('/index.css');
-</style>
-<div class="page-wrapper">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Centered Language Title</title>
+    <link rel="stylesheet" href="/index.css">
+</head>
+<body>
+    <div class="page-wrapper">
 
+        <!-- Hero -->
+        <section class="hero">
+            <div class="hero-title-container">
+                <h1 class="hero-title">Daan Jacobs</h1>
+                <p class="hero-subtext">Frontend Developer</p>
+            </div>
+            <div class="hero-arrow-container">
+                <img class="hero-arrow-img" src="/images/Arrow.png" alt="arrow down">
+            </div>
+        </section>
 
-<!-- Hero -->
-<section class="hero">
-<div class="hero-title-container">
-<h1 class="hero-title" >Daan Jacobs</h1>
-<p class="hero-subtext">Frontend Developer</p>
-</div>
-<div class="hero-arrow-container">
-<img class="hero-arrow-img" src="/images/Arrow.png" alt="arrow down">
-</div>
-</section>
-<!-- About -->
-<section class="languages">
+        <!-- Languages -->
+        <section class="languages">
+            <div class="language-title">
+                <h2>Programmeer Talen</h2>
+            </div>
+            <div class="languages-container">
+                <section class="languages-html">
+                    <h2>HTML</h2>
+                    <box-icon type='logo' name='html5' size='10em'></box-icon>
+                </section>
+                <section class="languages-css">
+                    <h2>CSS</h2>
+                    <box-icon name='css3' type='logo' size='10em'></box-icon>
+                </section>
+                <section class="languages-js">
+                    <h2>Javascript</h2>
+                    <box-icon name='javascript' type='logo' size='10em'></box-icon>
+                </section>
+                <section class="languages-node">
+                    <h2>Node JS</h2>
+                    <box-icon name='nodejs' type='logo' size='10em'></box-icon>
+                </section>
+                <section class="languages-php">
+                    <h2>PHP</h2>
+                    <box-icon name='php' type='logo' size='10em'></box-icon>
+                </section>
+                <section class="languages-mysql">
+                    <h2>MYSQL</h2>
+                    <box-icon name='postgresql' type='logo' size='10em'></box-icon>
+                </section>
+            </div>
+        </section>
 
-</section>
-<!-- Languages -->
-</div>
+    </div>
+</body>
+</html>
