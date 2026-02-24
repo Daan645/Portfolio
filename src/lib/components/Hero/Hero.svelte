2dd8f9cf9ccb5a3787be18ab13cca2a34efc3dba
Daan Jacobs <daan@videotechniek.nl>
2026-02-24 20:09:37 +0100

    fixed hero component

---
+++ src/lib/components/Hero/Hero.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { activateHtmlTags } from './Hero';
+
+	onMount(() => {
+		const button = document.getElementById('html-tags-button');
+		const tags = Array.from(document.querySelectorAll<HTMLElement>('.html-tags'));
+
+		if (!button) return;
+		activateHtmlTags(button, tags);
+	});
+</script>
+
+<!-- Hero -->
+<section class="hero">
+	<div class="hero-title-container">
+		<h1 class="hero-title">Daan Jacobs</h1>
+		<button id="html-tags-button" class="hero-subtext">Frontend Developer</button>
+	</div>
+	<div class="hero-arrow-container">
+		<img class="hero-arrow-img" src="/images/Arrow.png" alt="arrow down" />
+	</div>
+
+	<!-- Hidden easter egg -->
+	<span class="html-tags">&lt;/html&gt</span>
+	<span class="html-tags">&lt;/body&gt</span>
+	<span class="html-tags">&lt;/head&gt</span>
+	<span class="html-tags">&lt;/footer&gt</span>
+	<span class="html-tags">&lt;/h1&gt</span>
+	<span class="html-tags">&lt;/div&gt</span>
+	<span class="html-tags">&lt;/section&gt</span>
+</section>
+
+<style lang="scss">
+	@import './Hero.scss';
+</style>
---
+++ src/lib/components/Hero/Hero.ts
@@ -0,0 +1,6 @@
+// Hero.ts
+export function activateHtmlTags(button: HTMLElement, tags: HTMLElement[]) {
+	button.addEventListener('click', () => {
+		tags.forEach((tag) => tag.classList.add('html-tags-rain'));
+	});
+}
